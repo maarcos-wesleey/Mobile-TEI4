b894f692bb76d76d915082fc19de6412772b5840
maarcos-wesleey <maarcoswesleey@gmail.com>
2026-04-13 20:23:13 -0300

    Ajustes para correção

--- CampoMinado/App.js
+++ CampoMinado/App.js
@@ -2,28 +2,44 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import params from './src/params';
 import Field from './src/components/Field';
+import MineField from './src/components/MineField';
+import { createMinedBoard } from './src/functions';
+import { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.welcome}>Iniciando o Mines</Text>
-      <Text style={styles.instructions}>Tamanho da grade: 
-        {params.getRowsAmount()}x{params.getColumnsAmount()}
-      </Text>
-      <Field />
-      <Field opened /> 
-      <Field opened nearMines={1} />
-      <Field opened nearMines={2} />
-      <Field opened nearMines={3} />
-      <Field opened nearMines={6} />
-      <Field mined />
-      <Field mined opened />
-      <Field mined opened exploded />
-      <Field flagged />
-      <Field flagged opened />
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>Iniciando o Mines</Text>
+        <Text style={styles.instructions}>Tamanho da grade:
+          {params.getRowsAmount()}x{params.getColumnsAmount()}
+        </Text>
+
+        <StatusBar style="auto" />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
--- CampoMinado/src/functions.js
+++ CampoMinado/src/functions.js
@@ -1,8 +1,8 @@
 const createBoard = (rows, columns) => {
     return Array(rows).fill(0).map((_, row) => {
-        return Array(columns).fill(0).map((_, column))
+        return Array(columns).fill(0).map((_, column) => {
             return {
-                row, 
+                row,
                 column,
                 opened: false,
                 flagged: false,
@@ -10,6 +10,7 @@ const createBoard = (rows, columns) => {
                 exploded: false,
                 nearMines: 0
             }
+        })
     })
 }
 
@@ -17,13 +18,13 @@ const spreadMines = (board, minesAmount) => {
     const rows = board.length
     const columns = board[0].length
     let minesPlanted = 0
-
+    
     while (minesPlanted < minesAmount) {
         const rowSel = parseInt(Math.random() * rows, 10)
         const columnSel = parseInt(Math.random() * columns, 10)
 
         if (!board[rowSel][columnSel].mined) {
-            board[rowSel[columnSel].mined = true]
+            board[rowSel][columnSel].mined = true
             minesPlanted++
         }
     }
@@ -33,5 +34,6 @@ const createMinedBoard = (rows, columns, minesAmount) => {
     const board = createBoard(rows, columns)
     spreadMines(board, minesAmount)
     return board
-
 }
+
+export {createMinedBoard}
