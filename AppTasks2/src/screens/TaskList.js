0bb6a9fac1fcf6b8bdac52ddcd3169fc4d91dfe3
maarcos-wesleey <maarcoswesleey@gmail.com>
2026-05-25 20:20:25 -0300

    Atulização 25/25 - 1

--- AppTasks2/src/screens/TaskList.js
+++ AppTasks2/src/screens/TaskList.js
@@ -1,21 +1,22 @@
-import React, { Component } from "react";
+import React, { Component } from 'react'
 import { View, Text, ImageBackground, StyleSheet, FlatList, TouchableOpacity, Platform } from 'react-native'
 
+import commonStyles from '../commonStyles'
 import todayImage from '../../assets/imgs/today.jpg'
-import commonStyles from "../commonStyles";
 
 import Icon from 'react-native-vector-icons/FontAwesome'
 
-import moment from "moment";
+import moment from 'moment'
 import 'moment/locale/pt-br'
 
-import Task from "../components/Task";
+import Task from '../components/Task'
+import AddTask from './AddTask'
 
 export default class TaskList extends Component {
-
     state = {
         showDoneTasks: true,
-
+        showAddTask: true,
+        visibleTasks: [],
         tasks: [{
             id: Math.random(),
             desc: 'Comprar Livro de React Native',
@@ -26,11 +27,27 @@ export default class TaskList extends Component {
             desc: 'Ler Livro de React Native',
             estimateAt: new Date(),
             doneAt: null,
-        },]
+        }]
+    }
+
+    componentDidMount = () => {
+        this.filterTasks()
     }
 
     toggleFilter = () => {
-        this.setState({showDoneTasks: !this.state.showDoneTasks})
+        this.setState({ showDoneTasks: !this.state.showDoneTasks }, this.filterTasks)
+    }
+
+    filterTasks = () => {
+        let visibleTasks = null
+        if(this.state.showDoneTasks) {
+            visibleTasks = [...this.state.tasks]
+        } else {
+            const pending = task => task.doneAt === null
+            visibleTasks = this.state.tasks.filter(pending)
+        }
+
+        this.setState({ visibleTasks })
     }
 
     toggleTask = taskId => {
@@ -48,25 +65,25 @@ export default class TaskList extends Component {
         const today = moment().locale('pt-br').format('ddd, D [de] MMMM')
         return (
             <View style={styles.container}>
+                <AddTask isVisible={this.state.showAddTask}
+                onCancel={() => this.setState ({ showAddTask: false })}/>
                 <ImageBackground source={todayImage}
                     style={styles.background}>
-                     <View style={styles.iconBar}>
+                    <View style={styles.iconBar}>
                         <TouchableOpacity onPress={this.toggleFilter}>
-                            <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'} 
-                            size={20} color={commonStyles.colors.secondary}/>
+                            <Icon name={this.state.showDoneTasks ? 'eye' : 'eye-slash'}
+                                size={20} color={commonStyles.colors.secondary} />
                         </TouchableOpacity>
-                     </View>
+                    </View>
                     <View style={styles.titleBar}>
                         <Text style={styles.title}>Hoje</Text>
                         <Text style={styles.subtitle}>{today}</Text>
                     </View>
-
                 </ImageBackground>
                 <View style={styles.taskList}>
-                    <FlatList data={this.state.tasks}
+                    <FlatList data={this.state.visibleTasks}
                         keyExtractor={item => `${item.id}`}
-                        renderItem={({ item }) => <Task {...item} 
-                        toggleTask={this.toggleTask}/>} />
+                        renderItem={({item}) => <Task {...item} toggleTask={this.toggleTask} />} />
                 </View>
             </View>
         )
@@ -75,36 +92,36 @@ export default class TaskList extends Component {
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        flex: 1
     },
     background: {
-        flex: 3,
+        flex: 3
     },
     taskList: {
         flex: 7
     },
     titleBar: {
         flex: 1,
-        justifyContent: 'flex-end',
+        justifyContent: 'flex-end'
     },
     title: {
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.secondary,
         fontSize: 50,
         marginLeft: 20,
-        marginBottom: 20,
+        marginBottom: 20
     },
     subtitle: {
         fontFamily: commonStyles.fontFamily,
         color: commonStyles.colors.secondary,
         fontSize: 20,
         marginLeft: 20,
-        marginBottom: 30,
+        marginBottom: 30
     },
     iconBar: {
         flexDirection: 'row',
-        marginHorizontal: 30,
+        marginHorizontal: 20,
         justifyContent: 'flex-end',
-        marginTop: 50
-    },
-})
+        marginTop: 80
+    }
+});
