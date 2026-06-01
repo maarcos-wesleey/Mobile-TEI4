485e37681d38d664d3f838c890d5fe6a1cc5d1d9
maarcos-wesleey <maarcoswesleey@gmail.com>
2026-06-01 20:55:18 -0300

    Ajuste Swipeable

--- AppTasks2/index.js
+++ AppTasks2/index.js
@@ -1,8 +1,16 @@
-import { registerRootComponent } from 'expo';
+import 'react-native-gesture-handler'
+import React from 'react'
+import { registerRootComponent } from 'expo'
+import { GestureHandlerRootView } from 'react-native-gesture-handler'
 
-import TaskList from './src/screens/TaskList';
+import TaskList from './src/screens/TaskList'
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
-registerRootComponent(TaskList);
+function App() {
+    return (
+        <GestureHandlerRootView style={{ flex: 1 }}>
+            <TaskList />
+        </GestureHandlerRootView>
+    )
+}
+
+registerRootComponent(App)
